8cbc6b3a61fb3b26c9a63b1d205d39ca73e3b3dc
Derek Redfern <redfern.derek@gmail.com>
2013-02-22 13:13:31 -0500

    Fixing callbacks

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,39 @@
 $(function () {
+  var mouseovers = function() {
+    $('.removeList').on("mouseover",function () {
+      $('#'+this.parentNode.parentNode.id+' .alttext').html("Remove List");
+      return false;
+    });
+    $('.recommend').on("mouseover",function () {
+      $('#'+this.parentNode.parentNode.id+' .alttext').html("Get a Similar Track");
+      return false;
+    });
+    $('.share').on("mouseover",function () {
+      $('#'+this.parentNode.parentNode.id+' .alttext').html("Share List");
+      return false;
+    });
+    $('.icons').on("mouseout",function () {
+      $('#'+this.parentNode.id+' .alttext').html("<br>");
+      return false;
+    });
+    $('.remove').on("mouseover",function () {
+      $('#'+this.parentNode.parentNode.parentNode.id.split('body')[0]+'head .alttext').html("Remove Song");
+      return false;
+    });
+    $('.remove').on("mouseout",function () {
+      $('#'+this.parentNode.parentNode.parentNode.id.split('body')[0]+'head .alttext').html("<br>");
+      return false;
+    });
+    $('.add').on("mouseover",function () {
+      $('#'+this.parentNode.id.split('body')[0]+'head .alttext').html("Add Song");
+      return false;
+    });
+    $('.add').on("mouseout",function () {
+      $('#'+this.parentNode.id.split('body')[0]+'head .alttext').html("<br>");
+      return false;
+    });
+  };
+  mouseovers();
   var remove = function () {
     console.log(this.parentNode.parentNode.id);
     $.post("/edit", {action:"remove_song",id:this.parentNode.parentNode.id});
@@ -27,6 +62,7 @@ $(function () {
         $(element.parentNode).html(data);
         $('.remove').on("click",remove);
         $('.add').on("click",add);
+        mouseovers();
         return false;
       };
       $.post("/edit", {action:"add_song",title:title,artist:artist,id:this.parentNode.id.split('body')[0]},addSong);
@@ -40,42 +76,12 @@ $(function () {
       $("#"+element.parentNode.parentNode.id.split("head")[0]+"body").html(data);
       $('.remove').on("click",remove);
       $('.add').on("click",add);
+      mouseovers();
       return false;
     };
     console.log("recommend");
     $.post("/recommend",{playlist:this.parentNode.parentNode.id.split('head')[0]},addSong);
     return false;
   });
-  $('.removeList').on("mouseover",function () {
-    $('#'+this.parentNode.parentNode.id+' .alttext').html("Remove List");
-    return false;
-  });
-  $('.recommend').on("mouseover",function () {
-    $('#'+this.parentNode.parentNode.id+' .alttext').html("Get a Similar Track");
-    return false;
-  });
-  $('.share').on("mouseover",function () {
-    $('#'+this.parentNode.parentNode.id+' .alttext').html("Share List");
-    return false;
-  });
-  $('.icons').on("mouseout",function () {
-    $('#'+this.parentNode.id+' .alttext').html("<br>");
-    return false;
-  });
-  $('.remove').on("mouseover",function () {
-    $('#'+this.parentNode.parentNode.parentNode.id.split('body')[0]+'head .alttext').html("Remove Song");
-    return false;
-  });
-  $('.remove').on("mouseout",function () {
-    $('#'+this.parentNode.parentNode.parentNode.id.split('body')[0]+'head .alttext').html("<br>");
-    return false;
-  });
-  $('.add').on("mouseover",function () {
-    $('#'+this.parentNode.id.split('body')[0]+'head .alttext').html("Add Song");
-    return false;
-  });
-  $('.add').on("mouseout",function () {
-    $('#'+this.parentNode.id.split('body')[0]+'head .alttext').html("<br>");
-    return false;
-  });
+  
 });
