47bd6a8732a30b9901e9f9aa068c1e63b2fdf5cf
Derek Redfern <redfern.derek@gmail.com>
2013-02-22 15:40:16 -0500

    Bugfix

--- public/javascripts/friends.js
+++ public/javascripts/friends.js
@@ -1,7 +1,7 @@
 $(function () {
   $('.select').on("click",function () {
     console.log(this.id);
-    $.post("/edit", {action:"add_friend",playlist:this.parentNode.parentNode.id,friend:this.id,name:this.parentNode.id},function(data){
+    $.post("/edit", {action:"add_friend",playlist:this.parentNode.parentNode.parentNode.id,friend:this.id,name:this.parentNode.id},function(data){
       window.location.href = '/';
       return false;
     });
